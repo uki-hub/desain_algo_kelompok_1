104e68bfcc8198960423b2ee1872632645ad5645
uki-hub <marzukiaslb@gmail.com>
2021-06-03 17:09:47 +0700

    a

--- lib/pages/splash_screen/splash_screen.dart
+++ lib/pages/splash_screen/splash_screen.dart
@@ -4,7 +4,7 @@ import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatelessWidget {
   Future<void> _waitToLoad(BuildContext context) async {
-    await Future.delayed(Duration(seconds: 2));
+    await Future.delayed(Duration(seconds: 600));
     Navigator.of(context).push(
       PageTransition(
         type: PageTransitionType.fade,
@@ -20,9 +20,38 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _waitToLoad(context);
-    return Scaffold(
-      body: Center(
-        child: Icon(Icons.home),
+    return Material(
+      color: const Color(0xff333333),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              // height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Image(
+                image: AssetImage('assets/icons/icon.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+            Text(
+              'ARITMATIKA DAN BANGUN DATAR',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontWeight: FontWeight.bold,
+                fontSize: (MediaQuery.of(context).size.height * 0.1) * 0.3,
+              ),
+            ),
+            Text(
+              'by Kelompok 1',
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+                fontSize: (MediaQuery.of(context).size.height * 0.1) * 0.2,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
